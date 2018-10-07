a10756f34b11556de555c63d019a1a9be33566c6
Andrew Altunin <33705016+AltuninAndrew@users.noreply.github.com>
2018-10-07 20:53:26 +0300

    Update Crawler.cs
    added link grouping

--- TestPoligon/Crawler.cs
+++ TestPoligon/Crawler.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using System.Net;
@@ -23,37 +23,99 @@ namespace WebCrawler
 
     class Crawler
     {
-        private WebClient _webClient = new WebClient();
-        private string _patternDefault = "(?<=href=\").*?(?=\")";
+        private WebClient _webClient;
 
+        private const string PATTERN_DEFAULT = "(?<=href=\").*?(?=\")",
+        PATTERN_FOR_DOMEN = @"(?<=(http||https):\/\/).*";
+     
+        private readonly string _defaultUrl,_defaultDomen;
+        private List<string> _siteLinks,_otherLinks;
 
-        public List<String> GetAllLinkInPage(string url)
+         
+        public Crawler(string defaultUrl)
+        {
+            _defaultUrl = defaultUrl;
+            _webClient = new WebClient();
+            _defaultDomen = Regex.Match(defaultUrl,PATTERN_FOR_DOMEN).ToString();
+        }
+
+        /// <summary>
+        /// Get all links on page with default url
+        /// </summary>
+        /// <returns>list with all links on page</returns>
+        public List<String> GetAllLinkOnPage()
         {
             string tempPage;
             List<String> links = new List<string>();
 
             _webClient.Encoding = Encoding.UTF8;
-            tempPage = _webClient.DownloadString(url);
+            tempPage = _webClient.DownloadString(_defaultUrl);
 
-            MatchCollection matchCollection = Regex.Matches(tempPage, _patternDefault);
+            MatchCollection matchCollection = Regex.Matches(tempPage, PATTERN_DEFAULT);
 
             foreach (var element in matchCollection)
             {
                 var temp = element.ToString();
 
-                if (temp.StartsWith("/"))
+                if (!(temp == "/") && !string.IsNullOrEmpty(temp) && !(temp == "#")
+                    && !temp.Contains(".pdf") && !temp.Contains(".doc") && !temp.Contains(".docx") 
+                    && !temp.StartsWith("#"))
                 {
-                    // replace first '/' with an empty char
-                    temp = url + temp;
+                    if (temp.StartsWith("/"))
+                    {
+                        // replace first '/' with an empty char
+                        temp = _defaultUrl + temp;
+                    }
+                    links.Add(temp);
+
                 }
 
-                if (!(temp == "/") && !string.IsNullOrEmpty(temp) && !(temp == "#")
-                    && !temp.Contains(".pdf") && !temp.Contains(".doc") && !temp.Contains(".docx"))
-                    links.Add(temp);
             }
 
             return links;
         }
 
+        /// <summary>
+        /// Distribution of links related to the site and not related
+        /// </summary>
+        private void SortLinks(List<string> allLinks)
+        {
+            _siteLinks = new List<string>();
+            _otherLinks = new List<string>();
+            foreach (var element in allLinks)
+            {
+
+                if (element.Contains(_defaultDomen))
+                {
+                    _siteLinks.Add(element);
+                }
+                else
+                {
+                    _otherLinks.Add(element);
+                }
+            }
+        }
+
+
+        public void Craw()
+        {
+            SortLinks(GetAllLinkOnPage());
+
+            // for debug
+            Console.WriteLine("Site links: ");
+            foreach (var element in _siteLinks)
+            {
+                Console.WriteLine(element);
+            }
+
+            Console.WriteLine("\nOther links: ");
+            {
+                foreach (var element in _otherLinks)
+                {
+                    Console.WriteLine(element);
+                }
+            }
+
+        }
     }
 }
